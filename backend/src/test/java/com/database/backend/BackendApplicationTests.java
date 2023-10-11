d59be675f34a0053add2dea841bc9794c6809f35
haifenggo <haifeng.zh@hotmail.com>
2023-10-11 17:13:59 +0800

    增加redis配置，并简单测试

--- backend/src/test/java/com/database/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/database/backend/BackendApplicationTests.java
@@ -4,6 +4,9 @@ import com.database.backend.dao.UserDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Set;
 
 @SpringBootTest
 class BackendApplicationTests {
@@ -11,9 +14,18 @@ class BackendApplicationTests {
     @Autowired
     UserDao userDao;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     void testUserCount() {
         System.out.println(userDao.getUserCount());
+    }
+
+    @Test
+    void testRedis(){
+        Set keys = redisTemplate.keys("*");
+        System.out.println(keys);
     }
 
 
