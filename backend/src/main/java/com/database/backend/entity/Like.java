ce6ceb6e2ab5559ea9a20b1032b4b001af58c39f
haifenggo <haifeng.zh@hotmail.com>
2023-10-11 10:32:26 +0800

    创建实体类，使用spring jdbc进行简单测试

---
+++ backend/src/main/java/com/database/backend/entity/Like.java
@@ -0,0 +1,19 @@
+package com.database.backend.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @描述
+ * @日期 2023/10/11 10:01
+ */
+@Data
+@NoArgsConstructor
+public class Like {
+    private Integer id;
+    private Integer userId;
+    private Integer postId;
+    private LocalDateTime createTime;
+}
---
+++ backend/src/main/java/com/database/backend/entity/LoginLog.java
@@ -0,0 +1,21 @@
+package com.database.backend.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @描述
+ * @日期 2023/10/11 9:54
+ */
+@Data
+@NoArgsConstructor
+public class LoginLog {
+    private Integer loginLogId;
+    private Integer userId;
+    private String username;
+    private String loginIp;
+    private Integer loginResult;
+    private LocalDateTime createTime;
+}
